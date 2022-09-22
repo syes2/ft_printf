7ab135e5a7e2a197ecbbbbfa852fc1bd2ff733fe
Seungyeon Baek <seungbae@c9r1s3.42seoul.kr>
2022-09-22 14:45:28 +0900

    final

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: seungbae <seungbae@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 16:22:00 by seungbae          #+#    #+#             */
-/*   Updated: 2022/09/19 13:21:37 by seungbae         ###   ########seoul.kr  */
+/*   Updated: 2022/09/21 15:09:04 by seungbae         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 # include <unistd.h>
 # include <stdarg.h>
 
+int	ft_printf(const char *format, ...);
+int	type_check(const char type, va_list *ap);
 int	print_str(char *str);
 int	print_nbr(int num);
 int	print_unb(unsigned int num);
 int	print_char(char c);
 int	print_hex(unsigned int num, char c);
-int	type_check(const char type, va_list *ap);
-int	ft_printf(const char *format, ...);
 int	print_ptr(unsigned long long num);
 
 #endif
